467c15906831a397233d3749d4bf9cf62e30c8d9
anicacio <anderson@nicacio.com.br>
2020-02-05 18:09:32 -0300

    Adicionados 3 alunos

---
+++ Aulas_WebFullStack/digitalHouse.js
@@ -0,0 +1,36 @@
+const escola = "Digital House";
+
+let alunos = [
+    {
+        nome: "Anderson Nicácio",
+        boletim: [
+            {
+                curso: "Full Stack",
+                notas: [8,9,7],
+                faltas: 0
+            }
+        ]
+    },
+    {
+        nome: "Andressa Fazam",
+        boletim: [
+            {
+                curso: "Full Stack",
+                notas: [8,10,7],
+                faltas: 1
+            }
+        ]
+    },
+    {
+        nome: "Anderson Ricardo",
+        boletim: [
+            {
+                curso: "Full Stack",
+                notas: [7,7,6],
+                faltas: 5
+            }
+        ]
+    }
+];
+
+console.log(alunos);
