b849398676cf8527a17a499c5db1afbf32e38092
anicacio <anderson@nicacio.com.br>
2020-02-08 19:09:32 -0300

    Adicionado checarAprovado

--- Aulas_WebFullStack/dojo.js
+++ Aulas_WebFullStack/dojo.js
@@ -3,19 +3,20 @@ const escola = "Digital House";
 let alunos = [{
         nome: "Anderson Nicácio",
         boletim: [{
-            nomeCurso: "Full Stack",
-            campus: "Vila Olimpia",
-            notas: [8, 9, 7],
-            faltas: 0,
-            dataMatricula: "Mon Jan 20 2020"
-        },
-        {
-            nomeCurso: "DevOps",
-            campus: "Vila Olimpia",
-            notas: [8, 9, 7],
-            faltas: 0,
-            dataMatricula: "Mon Jan 20 2020"
-        }]
+                nomeCurso: "Full Stack",
+                campus: "Vila Olimpia",
+                notas: [8, 9, 7],
+                faltas: 0,
+                dataMatricula: "Mon Jan 20 2020"
+            },
+            {
+                nomeCurso: "DevOps",
+                campus: "Vila Olimpia",
+                notas: [7, 9, 10],
+                faltas: 10,
+                dataMatricula: "Mon Jan 20 2020"
+            }
+        ]
     },
     {
         nome: "Andressa Fazam",
@@ -55,15 +56,14 @@ const listarAlunos = () => {
 
 const matricularAluno = (nomeAluno, novoCurso, campus) => {
     let alunoExistente = alunos.filter(aluno => (aluno.nome == nomeAluno));
-    const verificarMatriculado = () =>{
-        if(alunoExistente.length != 0){
+    const verificarMatriculado = () => {
+        if (alunoExistente.length != 0) {
             let boletim = alunoExistente[0].boletim.filter(curso => (curso.nomeCurso == novoCurso));
             return ((boletim.length == 0) ? false : true);
         }
     };
-    if(alunoExistente.length != 0){
-        console.log(alunoExistente.length)
-        if(!verificarMatriculado()){
+    if (alunoExistente.length != 0) {
+        if (!verificarMatriculado()) {
             alunoExistente[0].boletim.push({
                 nomeCurso: novoCurso,
                 campus: campus,
@@ -85,21 +85,36 @@ const matricularAluno = (nomeAluno, novoCurso, campus) => {
                 faltas: 0,
                 dataMatricula: new Date().toDateString()
             }]
-        })
-        console.log(`${nomeAluno} cadastradx e matriculadx no curso ${novoCurso}!!!`)
-    }
-
+        });
+        console.log(`${nomeAluno} cadastradx e matriculadx no curso ${novoCurso}!!!`);
+    };
 };
 
-// matricularAluno("Daniele de Oliveira Lucas", "Full Stack", "Vila Olimpia");
-matricularAluno("Anderson Nicácio", "Full Stack", "Vila Olimpia");
+const checarAprovado = (nomeAluno) => {
+    let alunoExistente = alunos.filter(aluno => (aluno.nome == nomeAluno));
+    const calcularMedia = () => {
+        let soma = curso.notas.reduce((pilha, nota) => {
+            return pilha + nota;
+        });
+        return (soma / curso.notas.length);
+    };
+    if (alunoExistente.length != 0) {
+        console.log(`Aluno: ${alunoExistente[0].nome}`);
+        for (curso of alunoExistente[0].boletim) {
+            let soma = calcularMedia();
+            console.log(`Curso: ${curso.nomeCurso} 
+                Nota: ${soma.toFixed(1)}
+                Situação: ${(soma.toFixed(1) > 7 && curso.faltas < 3) ? "Aprovado" : "Reprovado"}`);
+        }
+    } else {
+        console.log("Aluno não encontrado!");
+    };
+};
 
-
-
-// console.log(alunos);
 // listarAlunos();
-// calcularMedia();
+// matricularAluno("Daniele de Oliveira Lucas", "Full Stack", "Vila Olimpia"); // Novo Aluno
+// matricularAluno("Anderson Nicácio", "Full Stack", "Vila Olimpia"); // Aluno Existente
+// checarAprovado("Daniele de Oliveira Lucas"); // Novo Aluno
+// checarAprovado("Anderson Nicácio"); // Aluno Existente
 // adicionarNota();
 // buscarAlunoPorNome();
-// checarAprovado();
-// matricularAluno("Anderson Nicácio", "Full Stack");
