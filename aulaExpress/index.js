e90bee0320120a25ba14152f5f9e497df5c10fa6
Anderson Nicacio <43139753+anicacio@users.noreply.github.com>
2020-02-19 16:54:20 -0300

    Alterado consolelog de inicio

--- aulaExpress/index.js
+++ aulaExpress/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const petRotas = require("./routes/pet");
 const app = express();
 app.listen(3000, () => {
-    console.log("tô ouvindo!")
+    console.log("Servidor Iniciado.")
 });
 app.use(petRotas);
